af6e80181eb33b6830f889ef2a40fc931636a53d
Pablo Fernandez <fernandez.pablo44@gmail.com>
2016-10-28 17:45:51 -0300

    Login creado

---
+++ View/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="./assets/css/style.css">
+
+		
+	</head>
+	<body>
+		<div class="container-fluid">
+		<br><br><br><br><br><br>
+			<div class="row">
+				<div class="col-md-4 col-md-offset-1">
+					<h2>Bienvenido</h2>
+					<h4>Sistema de Gestion de Cambios</h4>
+				</div>
+				<div class="col-md-4 col-md-offset-1 panel panel-default panelLogin">
+					<form action="" method="POST" role="form">
+						<div class="form-group">
+							<label for="user">Usuario</label>
+							<input type="text" class="form-control" id="user" placeholder="USUARIO...">
+							<br>
+							<label for="pass">Contraseña</label>
+							<input type="password" class="form-control" id="pass" placeholder="CONTRASEÑA...">
+						</div>
+						<button type="INGRESAR" class="btn btn-primary">INGRESAR</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		<!-- jQuery -->
+		<script src="./assets/js/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="./assets/js/bootstrap.min.js" ></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+
+	</body>
+</html>
