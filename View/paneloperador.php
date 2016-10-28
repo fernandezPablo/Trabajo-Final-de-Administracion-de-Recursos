6d0b85e2052fa9ac8371a0ba21b8b18e5e04a7bc
Pablo Fernandez <fernandez.pablo44@gmail.com>
2016-10-28 18:25:24 -0300

    Listado de cambios

---
+++ View/paneloperador.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+		
+		
+	</head>
+	<body>
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-5 col-md-offset-1">
+					<h5>Solicitudes de Cambio</h5>
+					<div class="table-responsive panel panel-default">
+						<table class="table table-hover">
+							<thead>
+								<tr>
+									<th>Origen</th>
+									<th>Solicitante</th>
+									<th>Vencmiento</th>
+									<th>Prioridad</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<?php 
+
+										 ?>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="col-md-5 ">
+					<h5>Información del Cambio</h5>
+					<div class="panel panel-default">
+						<div class="row">
+							<div class="col-md-12">
+								<p>DESCRIPCION</p>
+								<p>MOTIVO</p>
+								<p>PROPOSITO</p>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="col-md-5">
+								<p>SOLICITANTE</p>
+								<p>TIEMPO ESTIMADO</p>
+								<p>VENCIMIENTO</p>
+								<p>EQUIPO</p>
+								<p>IMPACTO</p>
+								<p>PRIORIDAD</p>
+							</div>
+							<div class="col-md-5 col-md-offset-1">
+								<p class="text-right">	
+									Luna, Sixto
+									<?php  ?>
+								</p>
+								<p>
+									<?php  ?>
+								</p>
+								<p>
+									<?php  ?>
+								</p>
+								<p>
+									<?php  ?>
+								</p>
+								<p>
+									<?php  ?>
+								</p>
+								<p>
+									<?php  ?>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- jQuery -->
+		<script src="./assets/js/jquery.min.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="./assets/js/bootstrap.min.js" ></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	</body>
+</html>
