70acf536177acd62e28bf41fb3f86483d8765d90
Pablo Fernandez <fernandez.pablo44@gmail.com>
2016-11-16 00:20:29 -0300

    prueba de agregar usaurio en el archivo bajaUsuario

--- View/BajaUsuario.php
+++ View/BajaUsuario.php
@@ -1,3 +1,11 @@
+<?php 
+	include("../Model/Usuario.php");
+	include("../Model/Perfil.php");
+
+
+	$user1 = new Usuario("Martin Palermo","El_Loco9","titan",new Perfil(1,"Administrador"));
+
+ ?>
 <p id="title" name="eliminarusuario">ELIMINAR USUARIO</p>
 <hr>
 <table class="table table-condensed table-hover panel-default panel">
@@ -36,6 +44,16 @@
 				<a href="">Eliminar</a>
 			</th>
 		</tr>
+
+		<tr>
+			<td><?php echo $user1->getNombreUsuario(); ?></td>
+			<td><?php echo $user1->getApellidoYNombre(); ?></td>
+			<td><?php echo $user1->getPerfil()->getNombrePerfil(); ?></td>
+			<th>
+				<a href="">Eliminar</a>
+			</th>
+		</tr>
+
 	</tbody>
 </table>
 					
